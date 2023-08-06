63e78cee9a8a9ad61ab7a275512c5b546707e7d6
bgfxc4 <bindermarc@gmx.de>
2023-08-06 21:10:55 +0200

    better perft output format

--- engine/tests/perft.rs
+++ engine/tests/perft.rs
@@ -3,13 +3,13 @@ use smogfish::board::Board;
 #[test]
 fn run_perft() {
     let b = Board::new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    // let b = Board::new("rnbqkbnr/pppppppp/8/8/8/3P4/PPP1PPPP/RNBQKBNR b KQkq - 0 1");
-    // let b = Board::new("rnbqkbnr/ppppppp1/7p/8/8/3P4/PPP1PPPP/RNBQKBNR w KQkq - 0 2");
-    let nodes = perft(5, b);
+    let nodes = perft(5, b, 5);
     println!("{} nodes", nodes);
 }
 
-fn perft(depth: u8, b: Board) -> u64 {
+const NUMBER_TO_CHAR: [char; 8] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+fn perft(depth: u8, b: Board, start_depth: u8) -> u64 {
     let mut nodes: u64 = 0;
 
     if depth == 0 {
@@ -20,15 +20,15 @@ fn perft(depth: u8, b: Board) -> u64 {
     for m in moves {
         let mut b_ = b.clone();
         b_.make_move(&m);
-        let n = perft(depth - 1, b_);
+        let n = perft(depth - 1, b_, start_depth);
         nodes += n;
-        if depth == 5 {
+        if depth == start_depth {
             println!(
-                "move {} {} to {} {}, {} nodes",
-                m.from.file(),
-                m.from.rank(),
-                m.to.file(),
-                m.to.rank(),
+                "{}{}{}{}: {}",
+                NUMBER_TO_CHAR[m.from.file() as usize],
+                m.from.rank() + 1,
+                NUMBER_TO_CHAR[m.to.file() as usize],
+                m.to.rank() + 1,
                 n
             );
         }
